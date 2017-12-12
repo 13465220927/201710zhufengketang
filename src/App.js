b5fe6a9227c2716fc945da2257117391d22d9930
zhufengnodejs <zhufengnodejs@126.com>
2017-12-12 12:51:25 +0800

    实现底部tab页

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Tab from "./components/Tab/index";
+import './style/comm.less'
 import {HashRouter as Router} from 'react-router-dom';
+//The prop `history` is marked as required in `Router`
+//如果是HashRouter hash
+//BrowserRouter html5 pushState
+//Router本身需要外界传入history
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div>
-
           <Tab/>
         </div>
       </Router>
