8e73f8027b6b1126bea62f93a2a9a8dde834bdcb
zhufengnodejs <zhufengnodejs@126.com>
2017-12-12 16:18:01 +0800

    引入轮播组件

--- src/containers/Home/HomeSlider/index.js
+++ src/containers/Home/HomeSlider/index.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 import ReactSwipe from 'react-swipe';
 export default class HomeSlider extends Component {
   render() {
+    let swipeOptions = {
+      auto: 3000,//加载完成后等待多少毫秒开始轮播
+      continuous: true,//是否启动无缝轮播
+      callback: function(index, elem) {},//当动画完成后执行回调函数
+    };
     return (
-      <div className="home-sliders">
-        <ReactSwipe>
+      <div className="home-sliders" >
+        <ReactSwipe swipeOptions={swipeOptions}>
           <div>面板1</div>
           <div>面板2</div>
           <div>面板3</div>
