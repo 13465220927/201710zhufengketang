4669df4d577063f421052aff4d9326034a996a10
zhufengnodejs <zhufengnodejs@126.com>
2017-12-12 20:19:35 +0800

    使用轮播图

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware,compose } from 'redux';
 import reducers from './reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = applyMiddleware(thunk,promise,logger)(createStore)(reducers);
+let store = composeEnhancers(applyMiddleware(thunk,promise,logger))(createStore)(reducers);
+
+//let store = createStore(reducers,composeEnhancers(applyMiddleware(thunk,promise,logger)));
 window.store = store;
 export default store;
